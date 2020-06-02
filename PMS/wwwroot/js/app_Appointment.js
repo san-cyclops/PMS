f0546c5efbbf62d08951be08307d963e188c930e
sanjeewa <sancyclops@gmail.com>
2020-06-02 07:35:52 +0530

    added

--- PMS/wwwroot/js/app_Appointment.js
+++ PMS/wwwroot/js/app_Appointment.js
@@ -2,12 +2,12 @@
 
 app.service('logservice', function ($http) {
     this.save = function (sub) {
-        console.log("aaaaaaaaaaa", sub);
+        console.log("Appoinment---SAVE", sub);
         return $http({
             method: "POST",
             contentType: "application/json; charset=utf-8",
-            url: "/Data/Save?strJson=" + sub + "&type=CourseType"
-
+            data: sub,
+            url: "https://localhost:5001/EHealthCareAPI/Appoinment"
         });
     };
     this.update = function (sub, id) {
@@ -63,7 +63,14 @@ app.service('logservice', function ($http) {
             url: "https://localhost:5001/EHealthCareAPI/Doctor/" + traceId + "/" + hospitalID + "/" + authkey
         });
     };
-    
+    this.loadMedHistory = function (id, traceId, authkey) {
+        console.log("https://localhost:5001/EHealthCareAPI/Treatments/treatMenthistory/" + id + "/" + traceId  + "/" + authkey);
+        return $http({
+            method: "GET",
+            contentType: "application/json; charset=utf-8",
+            url: "https://localhost:5001/EHealthCareAPI/Treatments/treatMenthistory/" + + id + "/" + traceId + "/" + authkey
+        });
+    };
 });
 
 
@@ -126,8 +133,35 @@ app.controller('MainCtrl', function ($scope, $timeout, $q, $window, $http, logse
                 } else {
                     newPerson = vm.model;
                     console.log("xxxxx", newPerson);
-                    var JsonString = JSON.stringify(newPerson);
-                    var savejson = vm.model;
+
+                    var obj = {
+                        password: $scope.sessionKey.password,
+                        username: $scope.sessionKey.userName,
+                        patientMasterDataID: 0,
+                        userStatus: "",
+                        patientData: {
+                            surname: "",
+                            name: "",
+                            dateOfBirth: "",
+                            gender: "",
+                            homeAddress: "",
+                            depentes: "",
+                            workAdderss: "",
+                            mobileNumber: "",
+                            homeNumber: "",
+                            officeNumber: "",
+                            imageUrl: "",
+                            officeEmail: "",
+                            email: "",
+                            alias: "",
+                            patientDataID: 0,
+                            patientMasterDataID: 0
+                        },
+                        authkey: $scope.sessionKey.authKey,
+                        traceId: "102030"
+
+                    }
+
 
                     var saveData = logservice.save(JsonString)
 
@@ -331,15 +365,36 @@ app.controller('MainCtrl', function ($scope, $timeout, $q, $window, $http, logse
                             });
                         }
                           
- 
-
                     }, function (error) {
                         console.log("Oops! Something went wrong while fetching the data.");
                     });
                    
                     $scope.filterExpression = function (doctor) {
-                        return (doctor.hospitalID === $scope.person.hospitalID);
+                        return (doctor.hospitalID === $scope.Patient.hospital.hospitalID);
                     };
+
+                    //Load Medical History ---------------------------
+
+                    $scope.medHistory = [];
+                    var loadMedHistory = logservice.loadMedHistory($scope.id,$scope.traceId,$scope.sessionKey.authKey)
+                    loadMedHistory.then(function (d) {
+
+                        console.log("Succss - ", d.data);
+
+                        var len = d.data.length;
+                        console.log("length", d.data.length);
+
+                        console.log("length", d.data.length);
+                        for (var i = 0; i < len; i++) {
+                            console.log(i, "---", d.data[i]);
+                            $scope.medHistory.push(d.data[i]);
+                        }
+                        console.log("medHistory", $scope.medHistory);  
+
+                    }, function (error) {
+                        console.log("Oops! Something went wrong while fetching the data.");
+                    });
+
  
 
                 }, function (error) {
@@ -389,5 +444,102 @@ app.controller('MainCtrl', function ($scope, $timeout, $q, $window, $http, logse
     };
 
     vm.pageLoad();
+
+
+    $scope.saveData = function () {
+        let isValiForSaving = false;
+        for (let propertyName in $scope.Patient) {
+            if ($scope.Patient[propertyName].length > 0) {
+                isValiForSaving = true;
+            }
+        }
+
+        if (isValiForSaving) {
+            let newPerson = {};
+
+            
+                if (isEditing !== false) {
+
+                    console.log("aa", isEditing);
+                    console.log("data", ccc);
+
+                    var editmodel = vm.model;
+
+                    var JsonEditString = JSON.stringify(vm.model);
+
+                    var updateData = logservice.update(JsonEditString, vm.model.ID)
+                    var result = false;
+
+                    updateData.then(function (d) {
+
+                        console.log("Update - Succss", editmodel);
+                        console.log("vm.model", vm.model);
+
+
+
+                        addressCollection[isEditing] = editmodel;
+                        isEditing = false;
+                        result = true;
+
+                    }, function (error) {
+                        result = false;
+                        console.log("Oops! Something went wrong while fetching the data.");
+                        alert("Oops! Something went wrong while fetching the data.");
+                    });
+
+                    //if (result === true) {
+                    //    console.log("Res");
+                    //    addressCollection[isEditing] = vm.model;
+                    //    isEditing = false;
+                    //};
+
+                } else {
+        
+                    console.log("xxxxx", $scope.Patient);
+
+                    var obj = {
+                        password: $scope.sessionKey.password,
+                        username: $scope.sessionKey.userName,
+                        TimeSlot: "",
+                        DoctorID: $scope.Patient.hospital.hospitalID,
+                        ProblemBrief: $scope.Patient.ProblemBrief,
+                        AppoinmentDate: $scope.Patient.AppoinmentDate,
+                        Age: $scope.Patient.Age.toString(),
+                        Patient: {
+                            PatientMasterDataID: $scope.id
+                        },
+                        Doctor: {
+                            DoctorID: $scope.Patient.hospital.hospitalID
+                        }, 
+                        Hospital: {
+                            HospitalID: $scope.Patient.hospital.hospitalID
+                        }, 
+                        authkey: $scope.sessionKey.authKey,
+                        traceId: "102030"
+                    }
+
+                    var JsonEditString = JSON.stringify(obj);
+                    console.log("JsonEditString--------", JsonEditString);
+
+                    var saveData = logservice.save(obj)
+
+                    saveData.then(function (d) {
+
+                        console.log("Succss");
+                        console.log("NewSAVE-----PERSON", d.data);
+                        //addressCollection.push(savejson);
+
+                    }, function (error) {
+                        console.log("Oops! Something went wrong while fetching the data.");
+                    });
+
+
+                }
+
+                vm.addresses = addressCollection;
+                vm.model = {};
+            
+        }
+    }
    
 });
