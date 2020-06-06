a680a763aee8068e91dc5272973a7407d36553c5
sanjeewa <sancyclops@gmail.com>
2020-06-06 19:04:52 +0530

    added

--- PMS/wwwroot/js/site.js
+++ PMS/wwwroot/js/site.js
@@ -1,6 +1,13 @@
-﻿//$("#login-button").click(function (event) {
-//	event.preventDefault();
-
-//	$('form').fadeOut(500);
-//	$('.wrapper').addClass('form-success');
-//});
+﻿function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
